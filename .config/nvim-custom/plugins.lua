adb0570232f8a657130d949faa5c462700a4b78e
blackgazzelle <blackgazzelle@pm.me>
2024-01-21 21:45:37 -0500

    getting colors to work with alacritty and adding conjure

--- .config/nvim-custom/plugins.lua
+++ .config/nvim-custom/plugins.lua
@@ -202,7 +202,38 @@ local plugins = {
     "jbyuki/venn.nvim",
     lazy = false,
   },
-
+  {
+    "Olical/conjure",
+    ft = { "clojure", "fennel", "python", "lua" }, -- etc
+    -- [Optional] cmp-conjure for cmp
+    dependencies = {
+      {
+        "PaterJason/cmp-conjure",
+        config = function()
+          local cmp = require("cmp")
+          local config = cmp.get_config()
+          table.insert(config.sources, {
+            name = "buffer",
+            option = {
+              sources = {
+                { name = "conjure" },
+              },
+            },
+          })
+          cmp.setup(config)
+        end,
+      },
+    },
+    config = function(_, opts)
+      require("conjure.main").main()
+      require("conjure.mapping")["on-filetype"]()
+    end,
+    init = function()
+      -- Set configuration options here
+      vim.g["conjure#debug"] = true
+      vim.g['conjure#extract#tree_sitter#enabled'] = true
+    end,
+  },
   -- To make a plugin not be loaded
   -- {
   --   "NvChad/nvim-colorizer.lua",
