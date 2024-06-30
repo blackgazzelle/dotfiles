df52beb3a01009a0cd3d19a99c984a560ecacfea
blackgazzelle <blackgazzelle@gmail.com>
2024-06-30 14:22:32 -0400

    updating bootstrap to add picom back and fixing neorg for new lazy.nvim

--- .config/nvim/lua/plugins/myplugins.lua
+++ .config/nvim/lua/plugins/myplugins.lua
@@ -301,8 +301,6 @@ local plugins = {
   },
   {
     "nvim-neorg/neorg",
-    dependencies = { "luarocks.nvim" },
-    lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
     version = "*", -- Pin Neorg to the latest stable release
     config = function()
       require("neorg").setup {
