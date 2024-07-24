a97290f4d64aa154cd742ee85f59121e90011570
blackgazzelle <blackgazzelle@gmail.com>
2024-07-23 21:46:15 -0400

    fixing clang filetypes

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local capabilities = require("nvchad.configs.lspconfig").capabilities
 local lspconfig = require "lspconfig"
 local util = require "lspconfig/util"
 -- if you just want default config for the servers then put them in a table
-local servers = { "html", "cssls", "tsserver", "clangd", "bashls", "marksman", "taplo", "bufls" }
+local servers = { "html", "cssls", "tsserver", "bashls", "marksman", "taplo", "bufls" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
@@ -47,4 +47,8 @@ lspconfig.ruff.setup {
     fixAll = true,
     organizeImports = true,
   },
+}
+
+lspconfig.clangd.setup {
+  filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "cc", "h", "hh", "hpp" },
 }
