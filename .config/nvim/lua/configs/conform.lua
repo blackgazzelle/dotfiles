2127c20d7f1eea8d1db901eb05e9f69f50718c01
blackgazzelle <blackgazzelle@gmail.com>
2024-09-29 16:01:02 -0400

    adding conform config

---
+++ .config/nvim/lua/configs/conform.lua
@@ -0,0 +1,28 @@
+local options = {
+  -- Define your formatters
+  formatters_by_ft = {
+    lua = { "stylua" },
+    css = { "prettierd", "prettier" },
+    html = { "prettierd", "prettier" },
+    json = { "prettierd", "prettier" },
+    python = { "ruff" },
+    markdown = { "mdformat" },
+    c = { "clang_format" },
+    cpp = { "clang_format" },
+    rust = { "rustfmt" },
+    cmake = { "cmake_format" },
+    yaml = { "yamlfix" },
+    toml = { "taplo" },
+    proto = { "buf" },
+  },
+
+  -- Set up format-on-save
+  ["_"] = { "trim_whitespace" },
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
+}
+
+return options
