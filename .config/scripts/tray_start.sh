0f8beeca0122d95c9d50e6beda24a8543a92f3eb
blackgazzelle <55161328+blackgazzelle@users.noreply.github.com>
2025-07-17 22:25:46 -0400

    udpating tray start

--- .config/scripts/tray_start.sh
+++ .config/scripts/tray_start.sh
@@ -2,6 +2,6 @@
 nm-applet &
 blueman-applet &
 nextcloud &
-if [ -x "$(command -v polychromatic-tray-applet-cli)" ]; then
+if [ -x "$(command -v polychromatic-tray-applet)" ]; then
     polychromatic-tray-applet &
 fi
